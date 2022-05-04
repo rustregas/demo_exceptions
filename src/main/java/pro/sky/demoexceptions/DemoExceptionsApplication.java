637a0708503bdec8843656584f87ead62c9e305e
Rustam Yusupov <yusupov@gnivc.ru>
2022-05-04 13:52:48 +0300

    first_commit

---
+++ src/main/java/pro/sky/demoexceptions/DemoExceptionsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.demoexceptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoExceptionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoExceptionsApplication.class, args);
+    }
+
+}
